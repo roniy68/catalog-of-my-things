1d77bb12e81a472af51ef07478d99b1d11f1f0de
ram1117 <ramkumark1@outlook.com>
2023-04-24 17:59:58 +0530

    add main.rb and app.rb classes

---
+++ modules/app.rb
@@ -0,0 +1,19 @@
+class App
+  def initalize
+  end
+  def evaluate_options(option)
+    case option
+    when 1
+    when 2
+    when 3
+    when 4
+    when 5
+    when 6
+    when 7
+    when 8
+    when 9
+    else
+      print "Please enter a valid option:\n"
+    end
+  end
+end
