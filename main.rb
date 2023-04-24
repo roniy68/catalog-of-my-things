e36325b7a396c5f8b5c4ea721c78eba5ca1cd81a
ram1117 <ramkumark1@outlook.com>
2023-04-24 18:47:32 +0530

    fix rubocop error

--- main.rb
+++ main.rb
@@ -2,14 +2,15 @@ require_relative './modules/app'
 
 def main
   menu = "Please select an option:\n[1] - All my books\n[2] - All my music albums\n[3] - All my games\n" \
-         "[4] - All genres\n" \
-         "[5] - All labels\n[6] - All authors\n[7] - Add a book\n[8] - Add a music album\n[9] - Add a game\n[10] - Exit\n" \
+         "[4] - All genres\n[5] - All labels\n[6] - All authors\n" \
+         "[7] - Add a book\n[8] - Add a music album\n[9] - Add a game\n[10] - Exit\n" \
          "Enter an option:\n"
   app = App.new
   while menu
     print menu
     user_option = gets.chomp.to_i
     break if user_option == 10
+
     app.evaluate_options(user_option)
   end
 end
