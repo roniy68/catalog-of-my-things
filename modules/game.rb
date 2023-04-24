239294db8b11b40f3516d341c773ea239c54646a
Lancelot-SO <fsowah001@gmail.com>
2023-04-24 15:13:24 +0000

    Add game and author class

---
+++ modules/game.rb
@@ -0,0 +1,11 @@
+require_relative 'item'
+class Game < Item
+    def initialize(multiplayer, last_played_at)
+        @multiplayer = multiplayer
+        @last_played_at = last_played_at
+    end
+
+    private
+    def can_be_archived?
+    end
+end
