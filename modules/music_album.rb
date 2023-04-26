1ec6e780a42e18e1e1b8fd796355d6fcd605d8be
ram1117 <ramkumark1@outlook.com>
2023-04-26 17:40:41 +0530

    modify parameters for music album class. remove data folder

--- modules/music_album.rb
+++ modules/music_album.rb
@@ -3,15 +3,13 @@ require_relative './item'
 class MusicAlbum < Item
   attr_accessor :on_spotify
 
-  def initialize(id, genre, author, source, label, published_date,
-                 on_spotify)
-    super(id, genre, author, source, label, published_date)
+  def initialize(name, published_date, on_spotify)
+    super(name, published_date)
     @on_spotify = on_spotify
   end
 
   def can_be_archived?
     return true if super && @on_spotify == true
-
     false
   end
 end
