59ae3371795343c4bbfe77aed55c1508a0f86932
roniy68 <ahmed.11900449@lpu.in>
2023-04-27 13:11:01 +0530

    🔥 Create Schema for genre and album

--- schema.sql
+++ schema.sql
@@ -36,3 +36,19 @@ CREATE TABLE game(
   multiplayer BOOLEAN,
   last_played_at DATE
 );
+
+CREATE TABLE album (
+  id INTEGER PRIMARY KEY,
+  source_id INTEGER,
+  label_id INTEGER REFERENCES label(id),
+  genre_id INTEGER,
+  author_id INTEGER,
+  published_date INTEGER,
+  archived BOOLEAN,
+  on_spotify BOOLEAN,
+)
+
+CREATE TABLE genre (
+    id INTEGER PRIMARY,
+    name VARCHAR,
+)
