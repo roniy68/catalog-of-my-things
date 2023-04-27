f3f61b1ee9212bae1541e662d6791f4443f1bc93
roniy68 <ahmed.11900449@lpu.in>
2023-04-27 13:22:29 +0530

    🔥 Add Music Album and validate

---
+++ modules/music_albums.rb
@@ -0,0 +1,33 @@
+require_relative './music_album'
+require_relative './validator'
+class MusicAlbums
+  include Validator
+  attr_accessor :albumlist
+
+  def initialize
+    @albumlist = []
+  end
+
+  def create_music_album
+    print "Enter Album name: \n"
+    name = validate_empty('Music Album')
+    print "Enter publisher name: \n"
+    publisher = validate_empty('Publisher')
+    print "Enter publish date [YYYY-MM-DD]: \n"
+    date = validate_date
+    print "Cover state [new/good/bad]: \n"
+    coverstate = validate_empty('Cover state [new/good/bad]')
+    add_book(
+      capitalize_text(name),
+      capitalize_text(publisher),
+      date,
+      coverstate
+    )
+  end
+
+  def add_music_album(name, publisher, date, coverstate)
+    music = MusicAlbum.new(name, date, publisher, coverstate)
+    @bookslist << book
+    book
+  end
+end
